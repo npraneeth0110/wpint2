ceb702154ab02bbb4d53f7aea9853d9d866242b1
N Praneeth <139494274+npraneeth0110@users.noreply.github.com>
2024-03-23 11:55:26 +0530

    Update and rename JdbcInsertion.java to One.java

--- One.java
+++ One.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.*;
 
-class JdbcInsertion {
+class One {
   public static void main(String[] args) throws Exception {
     Class.forName("oracle.jdbc.driver.OracleDriver");
     Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
